f44daae5383877167a0fa7b5287087bc13115371
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-02-03 08:38:17 +0300

    Write the code for function body

--- copy-sorted.js
+++ copy-sorted.js
@@ -1,2 +1,7 @@
 // We have an array of strings arr. We’d like to have a sorted copy of it, but keep arr unmodified.
 // Create a function copySorted(arr) that returns such a copy.
+
+function copySorted(arr) {
+    // using slice() to have a copy of the array
+  return arr.slice().sort();
+}
