6bfb026c9099aa81680ba0b4118b22725eafd2c0
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-02-03 07:38:34 +0300

    Add comments to explain task requirements

---
+++ filter.js
@@ -0,0 +1,3 @@
+// Write a function to filter a given array of numbers for values between and including two numbers
+// The task requires the function to have three parameters, one being the array and the other two being numbers
+// The function should not modify the array but return the new array
