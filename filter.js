12f32048d4d344656e89054612ff2420b9535f7d
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-02-03 07:49:30 +0300

    Add an example

--- filter.js
+++ filter.js
@@ -6,3 +6,8 @@ function filterRange(arr, a, b) {
   const filtered = arr.filter(n => (n >= a && n <= b));
   return filtered;
 }
+
+let arr = [1, 18, 4, 9, 12, 2];
+
+let result = filterRange(arr, 2, 10);
+console.log(result);
