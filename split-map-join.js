7ec7dc0123cfa07ab6632bdb0ced724f79dc276e
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-02-03 07:13:13 +0300

    Write the function body

--- split-map-join.js
+++ split-map-join.js
@@ -1,1 +1,14 @@
 // The task requires writing a function to transform strings with hyphen into camelCase 
+
+function camelize(string) {
+    return string
+        .split("-")
+        .map((item, index) => {
+            if (index === 0) {
+                return item;
+            } else {
+                return item[0].toUpperCase() + item.slice(1);
+            }
+        })
+        .join("");
+}
