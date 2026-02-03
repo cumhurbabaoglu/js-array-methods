f966fb80829f59a95143a67d6ff5a40c8c3b8ec1
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-02-03 08:19:23 +0300

    Add last comment

--- sort.js
+++ sort.js
@@ -5,3 +5,6 @@ let arr = [5, 2, 1, -10, 8];
 arr.sort((a, b) => b - a);
 
 console.log(arr); // [8, 5, 2, 1, -10]
+
+// sort() method treats numbers as strings by default
+// so we need to give sort() a function to deal with numbers
