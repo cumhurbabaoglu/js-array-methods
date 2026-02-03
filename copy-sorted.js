52a60afc629d5d34b235a59bb16793b6c42a42d6
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-02-03 08:40:07 +0300

    Add an example to test the code

--- copy-sorted.js
+++ copy-sorted.js
@@ -5,3 +5,10 @@ function copySorted(arr) {
     // using slice() to have a copy of the array
   return arr.slice().sort();
 }
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+console.log(sorted); // ['CSS', 'HTML', 'JavaScript']
+console.log(arr);   // ['HTML', 'JavaScript', 'CSS']
