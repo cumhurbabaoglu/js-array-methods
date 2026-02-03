a011b4dd846daf0c83b0913150fde4cff12f6b2a
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-02-03 07:47:29 +0300

    Add function body

--- filter.js
+++ filter.js
@@ -1,3 +1,8 @@
 // Write a function to filter a given array of numbers for values between and including two numbers
 // The task requires the function to have three parameters, one being the array and the other two being numbers
 // The function should not modify the array but return the new array
+
+function filterRange(arr, a, b) {
+  const filtered = arr.filter(n => (n >= a && n <= b));
+  return filtered;
+}
