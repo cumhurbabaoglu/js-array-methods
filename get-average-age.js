7277df684828a5afe3c9f469e12fb809b78e4deb
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-03-21 10:54:50 +0300

    Add the solution

--- get-average-age.js
+++ get-average-age.js
@@ -11,3 +11,17 @@
 // let arr = [ john, pete, mary ];
 
 // alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 29 };
+
+let arr = [ john, pete, mary ];
+
+function getAverageAge(array) {
+    const sumAges = array.reduce((acc, item) => acc + item.age, 0);
+    return sumAges / array.length;
+}
+
+console.log(getAverageAge(arr));
+// 28
