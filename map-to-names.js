67f363bf52f31ea878fd82b41a351ac13cd3ab9f
Cumhur Babaoğlu <cbabaoglu.dev@gmail.com>
2026-03-21 10:47:52 +0300

    Add the solution

--- map-to-names.js
+++ map-to-names.js
@@ -11,3 +11,12 @@
 // let names = /* ... your code */
 
 // alert( names ); // John, Pete, Mary
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [ john, pete, mary ];
+
+let names = users.map(user => user.name);
+console.log(names);
